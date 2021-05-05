8f0de58f81e51acbdf79bfb328822d350c63e523
DanielAlejandroGuerrieri <daniel.guerrieri@gmail.com>
2021-05-05 17:59:56 -0300

    Fix file upload

--- routes/uploadFile.js
+++ routes/uploadFile.js
@@ -3,10 +3,15 @@ const router = new express.Router();
 
 const upload = require('../middleware/uploadMiddleware');
 
+console.time('carga');
+
 router.post('/archivo', upload.single('file'), (req, res) => {
     console.log(req.body);
     console.log(req.file.originalname);
+
     res.status(200).send('anda...')
 });
+console.timeEnd('carga');
+
 
 module.exports = router
